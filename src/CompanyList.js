f7db4d4eee5fa580fffe8eba2a5c67bf2682d0fd
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-25 19:00:51 -0500

    added small html to test

--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,6 +1,10 @@
 
 function CompanyList () {
-
+  return (
+    <div className="CompanyList">
+      <h2>CompanyList</h2>
+    </div>
+    );
 };
 
 export default CompanyList;
--- src/JobList.js
+++ src/JobList.js
@@ -1,7 +1,11 @@
 
 
 function JobList () {
-
+  return (
+  <div className="JobList">
+    <h2>JobList</h2>
+  </div>
+  );
 };
 
 export default JobList;
