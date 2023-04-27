c907da361abf915fbee0faef0c42c2777ac3fc56
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-27 17:34:32 -0500

    added token decoding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import RoutesList from './RoutesList';
 import Navigation from './Navigation';
 import JoblyApi from './api';
 import userContext from './userContext';
+import jwt_decode from 'jwt-decode';
 
 /**
  *
@@ -21,12 +22,31 @@ import userContext from './userContext';
  */
 function App() {
 	const [currentUser, setCurrentUser] = useState(null);
+	const [userToken, setUserToken] = useState(null);
 
 	async function handleRegisterRequest(userData) {
-		const newUser = await JoblyApi.signUp(userData);
-		console.log('newUser', newUser);
-		setCurrentUser(newUser);
+		const token = await JoblyApi.signUp(userData);
+		setUserToken(token)
 	}
+
+	useEffect(
+    function fetchCurrentUserDataOnChange() {
+      async function fetchCurrentUserData() {
+        try {
+					const { username } = jwt_decode(userToken)
+					console.log("username", username);
+          setCurrentUser(await JoblyApi.getUser({ username: username }));
+
+        } catch (error) {
+
+        }
+      }
+      fetchCurrentUserData();
+    },
+    [userToken]
+  );
+
+
 
 	return (
 		<div className='App'>
