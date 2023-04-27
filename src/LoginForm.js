d2073e87f453c5a2ada8fa104f06223a44d2f401
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-04-27 13:55:37 -0700

    added login and signup form components

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 
 /**
  *
@@ -13,8 +14,56 @@
  *                      JobList, LoginForm, SignUpForm, ProfileForm }
  *
  */
-function LoginForm() {
+function LoginForm({handleRequest}) {
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+	});
 
+	function handleSubmit(evt) {
+    evt.preventDefault();
+    handleRequest(formData);
+  }
+
+	function handleChange(evt) {
+		const { name, value } = evt.target;
+		setFormData(fData => ({
+			...fData,
+			[name]: value,
+		}));
+	}
+
+	return (
+		<div className='SignUpForm'>
+			<h1>Log in</h1>
+			<div className='SignUpForm-card card'>
+				<form
+					id='SignUpForm-form'
+					onSubmit={handleSubmit}>
+					<div>
+						<label for='username'>Username</label>
+						<input
+							id='username'
+							name='username'
+							onChange={handleChange}></input>
+					</div>
+
+					<div>
+						<label for='password'>Password</label>
+						<input
+							id='password'
+							name='password'
+							type='password'
+							onChange={handleChange}></input>
+					</div>
+					<div>
+						<button type='submit'>Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default LoginForm;
+
--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 /**
  *
  * Renders sign up form and makes request to register user and login
@@ -12,8 +14,80 @@
  *                      JobList, LoginForm, SignUpForm, ProfileForm }
  *
  */
-function SignUpForm() {
+function SignUpForm({ handleRequest }) {
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+		firstName: '',
+		lastName: '',
+		email: '',
+	});
 
-};
+	function handleSubmit(evt) {
+    evt.preventDefault();
+    handleRequest(formData);
+  }
+
+	function handleChange(evt) {
+		const { name, value } = evt.target;
+		setFormData(fData => ({
+			...fData,
+			[name]: value,
+		}));
+	}
+
+	return (
+		<div className='SignUpForm'>
+			<h1>Sign up</h1>
+			<div className='SignUpForm-card card'>
+				<form
+					id='SignUpForm-form'
+					onSubmit={handleSubmit}>
+					<div>
+						<label for='username'>Username</label>
+						<input
+							id='username'
+							name='username'
+							onChange={handleChange}></input>
+					</div>
+
+					<div>
+						<label for='password'>Password</label>
+						<input
+							id='password'
+							name='password'
+							type='password'
+							onChange={handleChange}></input>
+					</div>
+					<div>
+						<label for='firstName'>First Name</label>
+						<input
+							id='firstName'
+							name='firstName'
+							onChange={handleChange}></input>
+					</div>
+					<div>
+						<label for='lastName'>Last Name</label>
+						<input
+							id='lastName'
+							name='lastName'
+							onChange={handleChange}></input>
+					</div>
+					<div>
+						<label for='email'>Email</label>
+						<input
+							id='email'
+							name='email'
+							type='email'
+							onChange={handleChange}></input>
+					</div>
+					<div>
+						<button type='submit'>Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
 
 export default SignUpForm;
